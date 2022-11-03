9a264a439067c0ad8e781293cf3a637e15870b02
ji-mmyliu <jimmyjcl753@gmail.com>
2022-11-03 11:28:39 -0400

    Add browser router

--- index.jsx
+++ index.jsx
@@ -1,8 +1,26 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Form } from "./components/form";
 import { MyList } from "./components/list";
 import { Parent } from "./components/moving-up";
 
+function App() {
+    return (
+        <BrowserRouter>
+            {/* Displayed on every route */}
+            <Link to="/form">Form</Link><br />
+            <Link to="/list">List</Link><br />
+            <Link to="/parent">Parent</Link><br />
+
+            <Routes>
+                <Route path="/form" element={<Form />} />
+                <Route path="/list" element={<MyList />} />
+                <Route path="/parent" element={<Parent />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MyList />);
+root.render(<App />);
